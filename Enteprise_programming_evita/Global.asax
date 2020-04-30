41f219ef707d8540b31c9d3857e86093f322bd35
Evita Broka <evita.broka.e27076@mcast.edu.mt>
2020-04-30 21:56:43 +0200

    Add project files.

---
+++ Enteprise_programming_evita/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Enteprise_programming_evita.MvcApplication" Language="C#" %>
---
+++ Enteprise_programming_evita/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Enteprise_programming_evita.Startup))]
+namespace Enteprise_programming_evita
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
